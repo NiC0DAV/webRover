5c494e479118c5e050cec4a04606d6df37851d44
brayanmartinez17 <63986071+brayanmartinez17@users.noreply.github.com>
2020-08-25 11:12:13 -0500

    Update infohiber.php

--- infohiber.php
+++ infohiber.php
@@ -17,6 +17,76 @@
         </button>
         </div>
     </nav>
+ <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  
+  <div class="row">
 
+    <div class="row col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+
+      <div class="card col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+        <img src="https://placehold.it/318x180/" class="card-img-top" alt="Card image" />
+
+        <div class="card-block">
+          <h4 class="card-title">Humedad</h4>
+          <p class="card-text">Each merged squad salts the wild porter. Should the charmed church dodge a camera? The gown litters the rhyme. Another discrete elevator defects over the target.</p>
+          <a href="http://sonarsystems.co.uk" class="btn btn-success">Button</a>
+        </div>
+      </div>
+
+      <div class="card col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
+        <img src="https://placehold.it/318x180/" class="card-img-top" alt="Card image" />
+
+        <div class="card-block">
+          <h4 class="card-title">Temperatura</h4>
+          <p class="card-text">Each merged squad salts the wild porter. Should the charmed church dodge a camera? The gown litters the rhyme. Another discrete elevator defects over the target.</p>
+          <a href="http://sonarsystems.co.uk" class="btn btn-success">Button</a>
+        </div>
+      </div>
+
+      <div class="card col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 .img-fluid">
+        <img src="https://placehold.it/318x180/" class="card-img-top" alt="Card image" />
+
+        <div class="card-block">
+          <h4 class="card-title">Otra variable</h4>
+          <p class="card-text">Each merged squad salts the wild porter. Should the charmed church dodge a camera? The gown litters the rhyme. Another discrete elevator defects over the target.</p>
+          <a href="http://sonarsystems.co.uk" class="btn btn-success">Button</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<br>
+<br>
+<br>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
 </body>
 </html>
