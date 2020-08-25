b2ae530c479e6e7523948584d7bd6b8800874130
nicodav <nicodavest@gmail.com>
2020-08-25 17:03:33 -0500

    Agrega modificaciones en clases de bootstrap para infohiber.php

--- infohiber.php
+++ infohiber.php
@@ -10,83 +10,86 @@
 	<title>Información Hibernadero</title>
 </head>
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a class="navbar-brand" href="#">Rover Agricultor</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        </div>
-    </nav>
- <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
-  
-  <div class="row">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top">
+    <a class="navbar-brand">Rover Agricultor</a>
+    <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div id="my-nav" class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="nav navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="#" >Cerrar Sesión</a>
+        </li>
+      </ul>
+    </div>
+  </nav>
 
-    <div class="row col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
 
+
+  <div class="container-fluid" style="margin-top:90px;">
+    <div class="row col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 ml-1 mt-5">
       <div class="card col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
         <img src="https://placehold.it/318x180/" class="card-img-top" alt="Card image" />
-
         <div class="card-block">
           <h4 class="card-title">Humedad</h4>
           <p class="card-text">Each merged squad salts the wild porter. Should the charmed church dodge a camera? The gown litters the rhyme. Another discrete elevator defects over the target.</p>
-          <a href="http://sonarsystems.co.uk" class="btn btn-success">Button</a>
+          <!-- <a href="http://sonarsystems.co.uk" class="btn btn-success">Button</a> -->
         </div>
       </div>
 
       <div class="card col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4">
         <img src="https://placehold.it/318x180/" class="card-img-top" alt="Card image" />
-
         <div class="card-block">
           <h4 class="card-title">Temperatura</h4>
           <p class="card-text">Each merged squad salts the wild porter. Should the charmed church dodge a camera? The gown litters the rhyme. Another discrete elevator defects over the target.</p>
-          <a href="http://sonarsystems.co.uk" class="btn btn-success">Button</a>
+          <!-- <a href="http://sonarsystems.co.uk" class="btn btn-success">Button</a> -->
         </div>
       </div>
 
       <div class="card col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 .img-fluid">
         <img src="https://placehold.it/318x180/" class="card-img-top" alt="Card image" />
-
         <div class="card-block">
           <h4 class="card-title">Otra variable</h4>
           <p class="card-text">Each merged squad salts the wild porter. Should the charmed church dodge a camera? The gown litters the rhyme. Another discrete elevator defects over the target.</p>
-          <a href="http://sonarsystems.co.uk" class="btn btn-success">Button</a>
+          <!-- <a href="http://sonarsystems.co.uk" class="btn btn-success">Button</a> -->
         </div>
       </div>
     </div>
   </div>
+<br>
+<br>
+<br>
+<div class="table-responsive">
+  <table class="table table-striped">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>Larry</td>
+        <td>the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
 </div>
-<br>
-<br>
-<br>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>Larry</td>
-      <td>the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
 </body>
 </html>
